1543f4095ff93bd4deea4c124588e089464cbe2b
unknown <theodeygas@gmail.com>
2023-12-09 17:48:24 +0100

    day 7

--- 2023/day7/day7_1.cpp
+++ 2023/day7/day7_1.cpp
@@ -12,7 +12,7 @@ int main() {
 
 
     struct comparator {
-        unordered_map<char, int> mp = {{'A',13}, {'K',12}, {'Q',11}, {'J',0}, {'T',9}, {'9',8}, {'8',7}, {'7',6}, {'6',5}, {'5',4}, {'4',3}, {'3',2}, {'2',1}};
+        unordered_map<char, int> mp = {{'A',13}, {'K',12}, {'Q',11}, {'J',10}, {'T',9}, {'9',8}, {'8',7}, {'7',6}, {'6',5}, {'5',4}, {'4',3}, {'3',2}, {'2',1}};
         inline bool operator() (const string& s1, const string& s2) {
             for (int i=0; i<s1.size(); i++) {
                 if (s1[i]!=s2[i]) return (mp[s1[i]] > mp[s2[i]]);
@@ -37,21 +37,19 @@ int main() {
         }
         bid_map[hand]=stoi(temp);
         unordered_map<char,int> count;
-        int j_count=0;
         for (auto letter:hand) {
-            if (letter=='J') j_count++;
-            else count[letter]++;
+            count[letter]++;
         }
        
         vector<int> freq;
         for (auto x:count) freq.push_back(x.second);
         sort(freq.begin(), freq.end(), greater<int>());
-        if (j_count==5||freq[0]+j_count==5) types[0].push_back(hand);
-        else if (freq[0]+j_count==4) types[1].push_back(hand);
-        else if (freq[0]+j_count==3&&freq[1]==2) types[2].push_back(hand);
-        else if (freq[0]+j_count==3&&freq[1]==1) types[3].push_back(hand);
-        else if (freq[0]+j_count==2&&freq[1]==2) types[4].push_back(hand);
-        else if (freq[0]+j_count==2&&freq[1]==1) types[5].push_back(hand);
+        if (freq[0]==5) types[0].push_back(hand);
+        else if (freq[0]==4) types[1].push_back(hand);
+        else if (freq[0]==3&&freq[1]==2) types[2].push_back(hand);
+        else if (freq[0]==3&&freq[1]==1) types[3].push_back(hand);
+        else if (freq[0]==2&&freq[1]==2) types[4].push_back(hand);
+        else if (freq[0]==2&&freq[1]==1) types[5].push_back(hand);
         else types[6].push_back(hand);
     }
     int rank=0;
